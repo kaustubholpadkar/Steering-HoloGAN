6d3090ac46929fcad698ee0154832fdff39f6a37
Kaustubh Olpadkar <kaustubh.olpadkar@gmail.com>
2019-11-29 02:23:04 -0500

    Update util.py

--- hologan/util.py
+++ hologan/util.py
@@ -18,8 +18,6 @@ def calc_mean_std(feat, eps=1e-5):
 
 
 def adaptive_instance_normalization(features, style_feat):
-    # print(features.size())
-    # print(style_feat.size())
     partition = style_feat.size()[1] // 2
     scale, bias = style_feat[:, :partition], style_feat[:, partition:]
     mean, variance = calc_mean_std(features)  # Only consider spatial dimension
@@ -30,12 +28,3 @@ def adaptive_instance_normalization(features, style_feat):
     normalized = scale_broadcast * normalized
     normalized += bias_broadcast
     return normalized
-
-# def adaptive_instance_normalization(content_feat, style_feat):
-#     assert (content_feat.size()[:2] == style_feat.size()[:2])
-#     size = content_feat.size()
-#     style_mean, style_std = calc_mean_std(style_feat)
-#     content_mean, content_std = calc_mean_std(content_feat)
-#
-#     normalized_feat = (content_feat - content_mean.expand(size)) / content_std.expand(size)
-#     return normalized_feat * style_std.expand(size) + style_mean.expand(size)
